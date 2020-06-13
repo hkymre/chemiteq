ae717b6d6f2d8ede155282be572248f234d6c2be
Mark Edgeley <mark@edgeley.net>
2020-06-13 10:41:58 +0000

    Initial commit

---
+++ src/env.js
@@ -0,0 +1,11 @@
+export const config = {
+    ENDPOINT: "https://v1.nocodeapi.com/hkymre/uptime/XSEbJyiEtkMjyJHM?monitors=780558519-780558524",
+
+    title: "Chemiteq status",
+
+    website: "https://chemiteq.com",
+
+    twitterHandle: "@Oxylpro",
+    
+    twitterURL: "https://twitter.com/Oxylpro"
+}
